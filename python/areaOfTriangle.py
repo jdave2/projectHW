78e83eb60988afd4b25c6bee101a1d83e5084e19
jdave2 <89554127+jdave2@users.noreply.github.com>
2021-11-22 12:30:32 -0500

    Create areaOfTriangle.py

---
+++ python/areaOfTriangle.py
@@ -0,0 +1,8 @@
+side_1 = 5
+sideTwo = 6
+SideTree = 7
+
+perimeter_of_triangle = (side_1 + sideTwo + SideTree) / 2
+
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
