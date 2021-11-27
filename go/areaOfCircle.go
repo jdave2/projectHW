1bf4bfbd66b9b5522879c7470c6a16a0c1d46044
jdave2 <89554127+jdave2@users.noreply.github.com>
2021-11-26 22:18:07 -0500

    Update areaOfCircle.go

--- go/areaOfCircle.go
+++ go/areaOfCircle.go
@@ -7,13 +7,13 @@ import "fmt"
 func main(){
     var rad float64
     const PI float64 = 3.14 // Constant
-    var area float64
+    var areaOfCircle float64
     var ci float64
     fmt.Print("Enter radius of Circle : ")
     fmt.Scanln(&rad)
-    area = PI * rad * rad 
-    fmt.Println("Area of Circle is : ",area)
-    ci = 2 * PI * rad
-    fmt.Println("Circumference of Circle is : ",ci)     
+    areaOfCircle = PI * rad * rad 
+    fmt.Println("Area of Circle is : ",areaOfCircle)
+    circ = 2 * PI * rad
+    fmt.Println("Circumference of Circle is : ",circ)     
 }
 
