b5986520fdd9e7be2f2d6a9b0c18bd9690036bd2
jdave2 <89554127+jdave2@users.noreply.github.com>
2021-11-26 22:09:51 -0500

    Update areaOfTriangle.py

--- python/areaOfTriangle.py
+++ python/areaOfTriangle.py
@@ -1,8 +1,7 @@
 side_1 = 5
 sideTwo = 6
-SideTree = 7
+SideThree = 7
 
-perimeter_of_triangle = (side_1 + sideTwo + SideTree) / 2
+perimeter_of_triangle = (side_1 + sideTwo + SideThree) / 2
 
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+print('The area of the triangle is %0.2f' %perimeter_of_triangle)
