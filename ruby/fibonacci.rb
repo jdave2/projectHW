cc5f4ec433b1ad1a14a5ce044fe8320f416518de
jdave2 <89554127+jdave2@users.noreply.github.com>
2021-11-26 22:20:13 -0500

    Update fibonacci.rb

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,6 +1,6 @@
-first=0
+first_=0
 second=1
-nextterm=0
+nxtTerm=0
 
 puts "Enter the number of terms:-"
 n=gets.chomp.to_i
@@ -9,12 +9,12 @@ puts "The first #{n} terms of Fibonacci series are:-"
 c=1
 while(c<=n+1)
 	if(c<=1)
-		nextterm=c
+		nxtTerm=c
 	else
-		puts nextterm
-		nextterm=first+second
-		first=second
-		second=nextterm
+		puts nxtTerm
+		nxtTerm=first_+second
+		first_=second
+		second=nxtTerm
 	end
 	c+=1
 end
